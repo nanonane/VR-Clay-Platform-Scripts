1d115a659ce598738171cf5a7196e1cf4667b753
Yinuo Zhu <202111998298@mail.bnu.edu.cn>
2024-05-06 16:14:17 +0800

    更新文件目录结构，在ServerMain中加入命令行调用IDP

--- Scripts/Networking/ServerMain.cs
+++ Scripts/Networking/ServerMain.cs
@@ -5,15 +5,34 @@ using System.Net.Sockets;
 using System.IO;
 using System.Text;
 using WindowsServer;
+using ShellCommand;
 
 int main()
 {
     WinServer server = new WinServer();
     server.setUp();
-    server.OnConnection();
-    server.receiveMessage();
-    server.sendMessage();
-    server.closeConnection();
+    try
+    {
+        while (true)
+        {
+            server.OnConnection();
+            while (true)
+            {
+                server.receiveMessage();
+                @"cd ~/repos/IDP/Projects/FEM/our_scripts
+                /usr/bin/python3.8 clay.py".Bash();
+                server.sendMessage();
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occured:" + ex.Message);
+    }
+    finally
+    {
+        server.closeConnection();
+    }
     return 0;
 }
 
